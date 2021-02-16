9590fa617ffeff6fb47fec366480f326e34ed29e
Paula <paulacsdo2gmail.com>
2021-02-16 09:41:05 +0100

    Clase principal actualizada con RESTA

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,8 @@
 
 import com.itt.calculadora.Suma; //importar la clase Suma para poder usar sus métodos
 import com.itt.calculadora.Cociente; //importar la clase Cociente para poder usar sus métodos
+import com.itt.calculadora.Resta;
+
 import java.util.Random;
 
 public class Principal {
@@ -79,7 +81,7 @@ public class Principal {
 		System.out.println("Cociente.dividirEnteros():\n--------------------------------------------------------");
 		int cocienteEntero1 = rand.nextInt(2000) - 1000;
 		int cocienteEntero2 = rand.nextInt(2000) - 1000;
-		double dividirEnteros = Cociente.dividirEnteros(sumaEntero1, sumaEntero2);
+		double dividirEnteros = Cociente.dividirEnteros(cocienteEntero1, cocienteEntero2);
 		System.out.println("oper1 = " + cocienteEntero1);
 		System.out.println("oper2 = " + cocienteEntero2);
 		System.out.println("Cociente.cocienteEnteros(oper1, oper2) = " + String.format("%.2f",dividirEnteros));
@@ -99,6 +101,52 @@ public class Principal {
 		double raizCuadrada = Cociente.raizCuadrada(entRaiz);
 		System.out.println("oper = " + String.format("%.2f",entRaiz));
 		System.out.println("Cociente.raizCuadrada(oper) = " + String.format("%.2f",raizCuadrada));
+		
+		/*
+		 * Código para demostrar el uso de los métodos de la clase Cociente
+		 */
+		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+		System.out.println("Demos de la clase Resta");
+		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+		System.out.println();
+		//1. Resta.restaReales()
+		System.out.println("Resta.restaReales():\n--------------------------------------------------------");
+		double restaReal1 = rand.nextInt(2000) - 1000 + rand.nextDouble();
+		double restaReal2 = rand.nextInt(2000) - 1000 + rand.nextDouble();
+		double restaReales = Resta.restaReales(restaReal1, restaReal2);
+		System.out.println("oper1 = " + String.format("%.2f", restaReal1));
+		System.out.println("oper2 = " + String.format("%.2f", restaReal2));
+		System.out.println("Resta.restaReales(oper1, oper2) = " +  String.format("%.2f", restaReales));
+		System.out.println("--------------------------------------------------------\n");		
+		//2. Resta.restaEnteros()
+		System.out.println("Resta.restaEnteros():\n--------------------------------------------------------");
+		int restaEntero1 = rand.nextInt(2000) - 1000;
+		int restaEntero2 = rand.nextInt(2000) - 1000;
+		int restaEnteros = Resta.restaEnteros(restaEntero1, restaEntero2);
+		System.out.println("oper1 = " + restaEntero1);
+		System.out.println("oper2 = " + restaEntero2);
+		System.out.println("Resta.restaEnteros(oper1, oper2) = " + restaEnteros);
+		System.out.println("--------------------------------------------------------\n");
+		//3. Resta.restaTres()
+		System.out.println("Resta.restaTres():\n--------------------------------------------------------");
+		restaReal1 = rand.nextInt(2000) - 1000 + rand.nextDouble();
+		restaReal2 = rand.nextInt(2000) - 1000 + rand.nextDouble();
+		double restaReal3 = rand.nextInt(2001) - 1000 + rand.nextDouble();
+		double restaTres = Resta.restaTres(restaReal1, restaReal2, restaReal3);
+		System.out.println("oper1 = " + String.format("%.2f", restaReal1));
+		System.out.println("oper2 = " + String.format("%.2f", restaReal2));
+		System.out.println("oper3 = " + String.format("%.2f", restaReal3));
+		System.out.println("Resta.restaTres(oper1, oper2, oper3) = " + String.format("%.2f", restaTres));
+		System.out.println("--------------------------------------------------------\n");
+		//4. Resta.restaAcumulada()
+		System.out.println("Resta.restaAcumulada():\n--------------------------------------------------------");
+		System.out.println("Estado inicial:\nresta_acumulada = " + Resta.resta_acumulada);
+		double restaAcum = rand.nextInt(2001) - 1000 + rand.nextDouble();
+		System.out.println("Cantidad a restar = " + String.format("%.2f", restaAcum));
+		Resta.restaAcumulado(restaAcum);;
+		System.out.println("Estado final:\nresta_acumulada = " + String.format("%.2f", Resta.resta_acumulada));
+		System.out.println("--------------------------------------------------------\n");
+		
 	}
 
 }
